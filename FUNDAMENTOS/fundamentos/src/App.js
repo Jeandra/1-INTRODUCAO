40e0eb7f74b7aa9d680e6516c5e5375f15836320
Jeandra <jeandra8789@gmail.com>
2023-11-01 16:21:11 -0300

    Update App.js

--- FUNDAMENTOS/fundamentos/src/App.js
+++ FUNDAMENTOS/fundamentos/src/App.js
@@ -1,13 +1,16 @@
 // components
 import FirstComponents from "./components/FirstComponents"; 
+import TemplateExpressions from "./components/TemplateExpression"; 
+
 // style css
 import './App.css';
 
 function App() {
   return (
     <div className="App">      
-      <h1>REACT FUNDAMENTOS</h1>
+      <h1>FUNDAMENTOS DE REACT</h1>
       <FirstComponents/>
+      <TemplateExpressions/>
     </div>
   );
 }
