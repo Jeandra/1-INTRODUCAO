022cec6cc266bde5989357591d8c551f60581745
Jeandra <jeandra8789@gmail.com>
2023-11-01 19:07:21 -0300

    setup de novo arquivo

---
+++ AVANÇANDO/avancado/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olaaa!</h1>
+      <div>
+       {/* Imagem em Public */} 
+       <img src="/Drink001.jpg" alt="Drink com fundo rosa" height='450px'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
