8914ed8cdedde88a1e4e5ab3a006a8b99b1db335
Jeandra <jeandra8789@gmail.com>
2023-11-01 15:59:55 -0300

    Update App.js

--- FUNDAMENTOS/fundamentos/src/App.js
+++ FUNDAMENTOS/fundamentos/src/App.js
@@ -1,12 +1,13 @@
-
-
+// components
+import FirstComponents from "./components/FirstComponents"; 
+// style css
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App">      
       <h1>REACT FUNDAMENTOS</h1>
+      <FirstComponents/>
     </div>
   );
 }
